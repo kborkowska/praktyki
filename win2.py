9a0dde0e26c903097384294d236673f0173d7715
Karoline <ka.borkowska@outlook.com>
2017-07-05 11:50:50 +0200

    v2

--- win2.py
+++ win2.py
@@ -3,37 +3,52 @@
 from PyQt4 import QtGui as pq
 from PyQt4 import QtCore as pc
 from PyQt4.QtCore import Qt as pcq
+
 from numpy import array, append
+
+import pyqtgraph as pg
+
 import sys
 
 class GUI(pq.QWidget):
+
+	w = 100
+	h = 100
+
 	def __init__(self):
 		super(GUI,self).__init__()
+		self.setSize()
 		self.createInitialData()
 		self.createSlider(1, 0, 10)
-		self.setSize()
+		self.createGraph()
 		self.show()
+
+	def createGraph(self):
+		global graph		
+		graph = pg.PlotWidget(self)
+		graph.plot(data)
+		graph.setGeometry(0,0,self.w-40,self.h)
 
 	def setSize(self):
 		desktop = pq.QDesktopWidget()
 		desktopGeometry = desktop.screenGeometry()
-		w = desktopGeometry.width()*2/5
-		h = desktopGeometry.height()*2/5
-		self.setGeometry(desktopGeometry.x(), desktopGeometry.y(), w, h)
+		self.w = desktopGeometry.width()*2/5
+		self.h = desktopGeometry.height()*2/5
+		self.setGeometry(desktopGeometry.x(), desktopGeometry.y(), self.w, self.h)
 
 	def setAndShowSize(self):
 		desktop = pq.QDesktopWidget()
 		desktopGeometry = desktop.screenGeometry()
-		w = desktopGeometry.width()*2/5
-		h = desktopGeometry.height()*2/5
+		self.w = desktopGeometry.width()*2/5
+		self.h = desktopGeometry.height()*2/5
 		#showSize used only for debugging purposes
 		self.showSize(desktopGeometry.x(), desktopGeometry.y(), desktopGeometry.width(), desktopGeometry.height(), "desktop", 20,20)
-		self.setGeometry(desktopGeometry.x(), desktopGeometry.y(), w, h)
-		self.showSize(desktopGeometry.x(), desktopGeometry.y(), w, h, "app", 20,50)
+		self.setGeometry(desktopGeometry.x(), desktopGeometry.y(), self.w, self.h)
+		self.showSize(desktopGeometry.x(), desktopGeometry.y(), "app", 20,50)
 
 	#showSize used only for debugging purposes
-	def showSize(self, x,y,w,h, name, sx, sy):
-		qStr = name+" geometry: x="+str(x)+", y="+str(y)+", w="+str(w)+", h="+str(h)
+	def showSize(self, x,y, name, sx, sy):
+		qStr = name+" geometry: x="+str(x)+", y="+str(y)+", w="+str(self.w)+", h="+str(self.h)
 		print(qStr)
 		geometryLabel = pq.QLabel(self)
 		geometryLabel.setText(qStr)
@@ -44,8 +59,10 @@ class GUI(pq.QWidget):
 		slider.setTickInterval(tickInterval)
 		slider.setMinimum(minimum)
 		slider.setMaximum(maximum)
-		
-#		slider.setGeeometry()
+		#slider.setTrackig(True)
+		slider.setTickPosition(pq.QSlider.TicksLeft)
+		sw = 40
+		slider.setGeometry(self.w-sw,0,sw,self.h)
 
 	def createInitialData(self):
 		global data
