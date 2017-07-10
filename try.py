06be75a05625da4c68615666724b9c7c128e1bc8
Karoline <ka.borkowska@outlook.com>
2017-07-10 15:10:56 +0200

    try.py has resizing implemented

--- try.py
+++ try.py
@@ -4,55 +4,152 @@ from PyQt4 import QtGui as pq
 from PyQt4 import QtCore as pc
 from PyQt4.QtCore import Qt as pcq
 from time import strftime
+from datetime import datetime as dt
 from numpy import array, append
-
+import numpy as np
 import pyqtgraph as pg
 
 import sys
 
-class timeAxisItem():
+class timeAxisItem(pg.AxisItem):
 
-	def timeRevind(self,initTime):
-		self.strings = []
-		currTime = strftime("%H:%M:%S")
-		secs = int(currTime[6:8])+int(currTime[3:5])*60+int(currTime[0:2])*3600
-		secs -= initTime
-		hurs = int(secs/3600)
-		secs -= hurs*3600
-		mins = int(secs/60)
-		secs -= mins*60
-		string = str(hurs)+":"+str(mins)+":"+str(secs)
-
-	def __init__(self, initTime,orientation="bottom"):
-		#super(timeAxisItem,self).__init__(orientation)
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		#print("jestem tu i nic nie pokzauje bo mam Cie w poważaniu")
 		#super.setStyle(textFillLimits = [(2,0.3)])
-		'''		
-		self.strings = []
-		currTime = strftime("%H:%M:%S")
-		for i in range(initTime,0,-1):
-			secs = int(currTime[6:8])
-			mins = int(currTime[3:5])
-			hurs = int(currTime[0:2])
-			secs -= initTime
-			if i == initTime:
-				print(currTime)
-				print(str(secs))
-			if secs<0:
-				mins += int(secs/60)
-				if i == initTime:
-					print(str(int(secs/60)))
-					print(str(mins))
-				if mins < 0:
-					hurs += int(mins/60)
-					if hurs < 0:
-						hurs = 24+hurs
-					mins = 60+mins
-				secs = 60+secs%60
-			string = str(hurs)+":"+str(mins)+":"+str(secs)
-			self.strings.append(string)
-		print(self.strings[0])
-		'''
+
+	def tickStrings(self, values, scale, spacing):
+		strings = []
+		#print("values:")
+		#print(values)
+		for i in values:
+			strings.append(self.timeToString(i))
+		#print("strings:")
+		#print(strings)
+		return strings
+
+	def timeToString(self,milsecs):
+		hurs = int(milsecs/3600000)
+		milsecs -= hurs*3600000
+		mins = int(milsecs/60000)
+		milsecs -= mins*60000
+		secs = int(milsecs/1000)
+		milsecs -= secs*1000
+		string = str(hurs)+":"+str(mins)+":"+str(secs)+":"+str(int(milsecs))
+		return string
+
+class GUI(pq.QWidget):
+
+	def __init__(self):
+		super(GUI,self).__init__()
+		self.setSize()
+		self.layout = pq.QHBoxLayout()
+		#self.layout.addStretch(0)
+		self.createInitialdata()
+		self.createSlider(1, 0, 10)
+		self.createGraph()
+		self.layout.addWidget(self.graph)
+		self.layout.addWidget(self.slider)
+		self.setLayout(self.layout)
+		self.show()
+
+	def createGraph(self):
+		self.maxX = 10 #10 for debugging purposes
+		self.graph = pg.PlotWidget(self,axisItems={'bottom': timeAxisItem(orientation='bottom')})
+		#aI = pg.AxisItem(orientation='bottom')
+		self.g = self.graph.plot(list(self.dataX),list(self.dataY))
+		#self.graph.setGeometry(0,0,self.width()-self.sw,self.height())
+		self.graph.resize(self.width()-self.sw,self.height())
+
+	def setSize(self):
+		desktop = pq.QDesktopWidget()
+		desktopGeometry = desktop.screenGeometry()
+		self.sw = 40
+		w = desktopGeometry.width()*2/5
+		h = desktopGeometry.height()*2/5
+		self.setGeometry(desktopGeometry.x(), desktopGeometry.y(), w, h)
+
+	def setAndShowSize(self):
+		desktop = pq.QDesktopWidget()
+		desktopGeometry = desktop.screenGeometry()
+		w = desktopGeometry.width()*2/5
+		h = desktopGeometry.height()*2/5
+		#showSize used only for debugging purposes
+		self.showSize(desktopGeometry.x(), desktopGeometry.y(), desktopGeometry.width(), desktopGeometry.height(), "desktop", 20,20)
+		self.setGeometry(desktopGeometry.x(), desktopGeometry.y(), w, h)
+		self.showSize(desktopGeometry.x(), desktopGeometry.y(), "app", 20,50)
+
+	#showSize used only for debugging purposes
+	def showSize(self, x,y, name, sx, sy):
+		qStr = name+" geometry: x="+str(x)+", y="+str(y)+", w="+str(self.w)+", h="+str(self.h)
+		print(qStr)
+		geometryLabel = pq.QLabel(self)
+		geometryLabel.setText(qStr)
+		geometryLabel.move(sx,sy)
+
+	def createSlider(self, tickInterval, minimum, maximum):
+		self.slider = pq.QSlider(pcq.Vertical,self)
+		self.slider.setTickInterval(tickInterval)
+		self.slider.setMinimum(minimum)
+		self.slider.setMaximum(maximum)
+		#slider.setTrackig(True)
+		self.slider.setTickPosition(pq.QSlider.TicksLeft)
+		#self.slider.setGeometry(self.width()-self.sw+self.geometry().x(),self.geometry().y(),self.sw,self.height())
+		self.slider.valueChanged.connect(self.updateGraph)
+		#slider.setStyleSheet()
+		self.slider.resize(self.sw,self.height())
+	
+	def updateGraph(self):
+		self.appenddata(self.sender().value())
+		#self.graph.clear()
+		#self.graph.plot(self.dataY)
+		self.g.setData(list(self.dataX),list(self.dataY))
+
+	def createInitialdata(self):
+		noOfFakeData = 5
+		self.dataX = self.createInitialTimeData(noOfFakeData)
+		self.dataY = np.zeros((noOfFakeData,), dtype=np.int)
+
+	def appenddata(self, x):
+		#currTime = strftime("%H:%M:%S.%f")
+		currTime = dt.now()
+		if self.dataY.size < self.maxX: 
+			self.dataY = append(self.dataY,x)
+			#self.dataX = append(self.dataX,int(currTime[6:8])+int(currTime[3:5])*60+int(currTime[0:2])*3600)
+			self.dataX = append(self.dataX,(currTime.hour*3600+currTime.minute*60+currTime.second)*1000+int(currTime.microsecond/1000))
+		else:
+			self.dataY = append(self.dataY[1:self.maxX],x)
+			#self.dataX = append(self.dataX[1:self.maxX],int(currTime[6:8])+int(currTime[3:5])*60+int(currTime[0:2])*3600)
+			self.dataX = append(self.dataX[1:self.maxX],(currTime.hour*3600+currTime.minute*60+currTime.second)*1000+int(currTime.microsecond/1000))
+		#print(self.dataY)
+
+	def createInitialTimeData(self,initTime):
+		#currTime = strftime("%H:%M:%S.%f")
+		currTime = dt.now()
+		times = []
+		for i in range(initTime-1,-1,-1):
+			times.append(self.timeRevind(i,currTime))
+		return times
+
+	def timeRevind(self,offsetTime, currTime):
+		tim = currTime.hour*3600+currTime.minute*60+currTime.second
+		tim -= offsetTime
+		tim = tim*1000+int(currTime.microsecond/1000)
+		return tim
+
+	def timeRevindFromString(self,offsetTime, currTime):
+		#self.strings = []
+		secs = int(currTime[6:8])+int(currTime[3:5])*60+int(currTime[0:2])*3600
+		secs -= offsetTime
+		return secs
+		
+
 if __name__ == "__main__":
-	tAI = timeAxisItem(3800)
-	tAI.timeRevind(3661)
+	style = pq.QStyleFactory.create("motif")
+	pq.QApplication.setStyle(style)
+	app = pq.QApplication(sys.argv)
+	gui = GUI()
+	app.exec_()
+	del gui
+	sys.exit()
 
